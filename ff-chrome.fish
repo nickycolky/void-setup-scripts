fff3ac699df688d848f289afb2e9cb0127f5309a
Nic <nic@void.mynet>
2025-05-22 18:42:57 +0100

    Deleted unnecessary files

--- ff-chrome.fish
+++
@@ -1,4 +0,0 @@
-function ff-chrome --wraps='doas nano .mozilla/firefox/5koc0r9i.default-default/chrome/firefox-gnome-theme/customChrome.css' --description 'alias ff-chrome=doas nano .mozilla/firefox/5koc0r9i.default-default/chrome/firefox-gnome-theme/customChrome.css'
-  doas nano .mozilla/firefox/5koc0r9i.default-default/chrome/firefox-gnome-theme/customChrome.css $argv
-        
-end
--- tb-chrome.fish
+++
@@ -1,4 +0,0 @@
-function tb-chrome --wraps='doas nano .thunderbird/lrh25pj1.default-default/chrome/userChrome.css' --description 'alias tb-chrome=doas nano .thunderbird/lrh25pj1.default-default/chrome/userChrome.css'
-  doas nano .thunderbird/lrh25pj1.default-default/chrome/userChrome.css $argv
-        
-end
